bc15fda00a8c971a47b8009ed4cab1f41794df15
feigiWiseman <f0583215276@gmail.com>
2025-12-21 23:40:17 +0200

    ADD NUMPY RGB HISTOGRAM TASK

---
+++ sign-up-main/numpy_implementation/task8.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# יצירת מערך (Array)
+data = np.array([10, 20, 30, 40, 50])
+
+# ביצוע חישובים
+average = np.mean(data)
+maximum = np.max(data)
+
+print(f"The average is: {average}")
+print(f"The maximum value is: {maximum}")
